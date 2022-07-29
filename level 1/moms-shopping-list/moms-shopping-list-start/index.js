81b2c9a99bee73afddae79a56bcbe14c63bd887f
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-07-29 16:33:52 -0500

    added arrow functions

--- level 1/moms-shopping-list/moms-shopping-list-start/index.js
+++ level 1/moms-shopping-list/moms-shopping-list-start/index.js
@@ -1,48 +1,48 @@
-// submit event 
+// submit event using arrow function
 const form = document.addItem
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+        event.preventDefault();
 
-    const input = form.title.value 
-    form.title.value = " "
+        const input = form.title.value;
+        form.title.value = " ";
 
-    // create new list items in ul
-    const ul = document.getElementById("list")
-    const li = document.createElement("li")
-    li.setAttribute("id", "item")
-    ul.append(li)
+        // create new list items in ul
+        const ul = document.getElementById("list");
+        const li = document.createElement("li");
+        li.setAttribute("id", "item");
+        ul.append(li);
 
-    //create list item name 
-    const div = document.createElement("div")
-    div.textContent = input
-    div.classList.add("newItem")
-    li.append(div)
+        //create list item name 
+        const div = document.createElement("div");
+        div.textContent = input;
+        div.classList.add("newItem");
+        li.append(div);
 
-    //create edit button for new list item
-    const edit = document.createElement("button")
-    edit.textContent = "Edit"
-    li.append(edit)
+        //create edit button for new list item
+        const edit = document.createElement("button");
+        edit.textContent = "Edit";
+        li.append(edit);
 
-    //create X button to delete a list item
-    const deleteBtn = document.createElement("button")
-    deleteBtn.textContent = "X"
-    deleteBtn.classList.add("delete")
-    deleteBtn.style.marginInline = "20px"
-    li.append(deleteBtn)
+        //create X button to delete a list item
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.classList.add("delete");
+        deleteBtn.style.margin = "20px";
+        li.append(deleteBtn);
 
-    //add action to our delete button 
-    deleteBtn.addEventListener("click", function(){
-        div.remove()
-        edit.remove()
-        deleteBtn.remove()
-        li.remove
-    })
+        //add action to our delete button 
+        deleteBtn.addEventListener("click", function () {
+            div.remove();
+            edit.remove();
+            deleteBtn.remove();
+            li.remove;
+        });
 
-    // edit contents of the list 
-    edit.addEventListener("click", function(event){
-        event.preventDefault()
-        div.contentEditable = "true"
+        // edit contents of the list using arrow function 
+        edit.addEventListener("click", (event) => {
+            event.preventDefault();
+            div.contentEditable = "true";
+        });
+
     });
-
-});
