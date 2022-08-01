c4b37f80a232d0f1dcab98799bcc04d569de72ca
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-01 12:19:21 -0500

    added switch statement

--- level 1/node-calculator/main.js
+++ level 1/node-calculator/main.js
@@ -1,1 +1,37 @@
-const readline = require("readline-sync")
+const readlineSync = require("readline-sync");
+
+let result;
+
+const num1 = readlineSync.question('Please enter your fist number: ');
+
+const num2 = readlineSync.queston('Please enter your second number: ');
+
+const operation = readlineSync.question('Please enter the operation to perform: add, sub, mul, div  ');
+
+switch (operation){
+    case "add":
+        result = num1 + num2;
+        console.log("The result is: " + `${num1} + ${num2} = ${result}`);
+        break;
+
+    case "sub":
+        result = num1 - num2;
+        console.log("The result is: " + `${num1} - ${num2} = ${result}`);
+        break;   
+
+    case "mul":
+        result = num1 * num2;
+        console.log("The result is: " + `${num1} * ${num2} = ${result}`);
+        break;
+
+    case "div":
+        result = num1 / num2;
+        console.log("The result is: " + `${num1} / ${num2} = ${result}`);
+        break;  
+
+    default:
+        console.log("Invalid input");
+        break;
+}
+
+
