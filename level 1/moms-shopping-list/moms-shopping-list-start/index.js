469b77d91dc953928aef5b673a58cb9ae9182b06
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-02 15:51:28 -0500

    edited function to delete button

--- level 1/moms-shopping-list/moms-shopping-list-start/index.js
+++ level 1/moms-shopping-list/moms-shopping-list-start/index.js
@@ -9,34 +9,35 @@ form.addEventListener("submit", (event) => {
 
         // create new list items in ul
         const ul = document.getElementById("list");
+
+        //create list item name 
         const li = document.createElement("li");
         li.setAttribute("id", "item");
         ul.append(li);
 
-        //create list item name 
         const div = document.createElement("div");
-        div.textContent = input;
+        li.textContent = input;
         div.classList.add("newItem");
-        li.append(div);
+        li.appendChild(div);
 
         //create edit button for new list item
         const edit = document.createElement("button");
         edit.textContent = "Edit";
-        li.append(edit);
-
+        li.appendChild(edit);
+    
         //create X button to delete a list item
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "X";
         deleteBtn.classList.add("delete");
         deleteBtn.style.margin = "20px";
-        li.append(deleteBtn);
+        li.appendChild(deleteBtn);
 
         //add action to our delete button 
         deleteBtn.addEventListener("click", function () {
-            div.remove();
-            edit.remove();
-            deleteBtn.remove();
-            li.remove;
+            //div.remove();
+            // edit.remove();
+            // deleteBtn.remove();
+            li.remove();
         });
 
         // edit contents of the list using arrow function 
