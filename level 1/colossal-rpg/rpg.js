f0d8cc2cf40972257d70ac55f438b7710ed9f447
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-17 19:35:15 -0500

    created note file to store project requirements

--- level 1/colossal-rpg/rpg.js
+++ level 1/colossal-rpg/rpg.js
@@ -1,19 +1,2 @@
-// We are traveling back in time to 1985. Our goal is to build a text-based RPG game.
-// ****** Project Requirements **********
+// see note file for ****** Project Requirements **********
 
-// 1. We must greet our user 
-// 2. Ask for user name and give them a chance to name thier player
-// 3. The user will be prompted to enter "w" to start walking
-// 4. We will run a random algorithm for everytime the user walks which will determine the chances of an enemy appearing 
-// - there will be 1/3 or 1/4 chance of being attack by a wild enemy
-// - in chance of an enemy appearing (1 out of 3 random): 
-// - user will can decide to ether ATTACK or RUN. 
-// - if ATTACKING: damage will be dealt between a min and max
-// - if RUNNING: 50% chance of escaping
-// - enemy will attack back for a random damage amount 
-// - we will use a LOOP while the player and enemy attack each other until one ether passes out or dies
-// - if player kills the enemy: player gets health points (HP) & a special item for their inventory. Player walks on
-// - if enemy kills the player: game ends with a "sad message" like "GAME OVER !! YOU'RE DEAD" 
-// 5. INVENTROY: 
-// - player gets an award if enemy is killed
-// - user will be able to enter "Print" or "p" to see their details: name, HP, and each inventroy item 
