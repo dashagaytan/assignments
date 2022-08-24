838efcd9c9b9c9968dc0f0559e4ca5ead8eb2ccc
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-08-23 20:31:22 -0500

    added walk, fight, run functions

--- level 1/colossal-rpg/rpg.js
+++ level 1/colossal-rpg/rpg.js
@@ -1,64 +1,70 @@
 // see note file for ****** Project Requirements ********** 
-
-// Intro: get users name and welcome them to the game 
-//****************************************************
-
 const readlineSync = require("readline-sync")
-const gamer = readlineSync.question("HELLO GAMER! What is your name? ")
-console.log(gamer)
-
-const welcome = readlineSync.question(`Welcome ${gamer}! You are about to enter The Forest of Death. As you travel through the dark, deep forest you'll have to defend your life from potential enemies.`)
-const playerName = readlineSync.question("Before we start, what do you want to name your player? ")
-console.log(playerName)
-
 let isAlive = true;
 
-// set up gamers information using constructor function  
-//***************************
+const user = readlineSync.question("Hello Gamer! What is your name? ")
+//console.log(user)
+const welcome = readlineSync.question(`Welcome ${user} to my RPG game. You are about to set out on a journey through THE DEATH FOREST.`)
+const name = readlineSync.question("Before we go any further...What would you like to name your player? ")
+//console.log(name)
 
-let player = {
-    name: playerName, 
-    hp: 100,
-    inventory: ["crossbow", "dagger", "axe"],
+const random = function(max, min){
+    return Math.floor((Math.random()* max)+min)
 }
 
-class Player { 
-    constructor(name, hp, inventory){
-        this.player = player,
-        this.name = playerName,
-        this.hp = hp,
-        this.inventory = ["crossbow", "dagger", "axe"]
-    }
+// player information
+const player = {
+    name: name,
+    hp: 50,
+    inventory: ["crossbow", "dagger", "axe"]
 }
-console.log(`${playerName} you are supplied with 100 hp and 3 weapons to help you defend yourself as you travel through the forest`)
 
-// set up enemy information using constructor function
-//**************************
-
-class  Enemy {
-    constructor(name, hp, enemyPrize){
+class Player{
+    constructor(name= "name",  hp = 50, inventory = []){
         this.name = name,
-        this.hp = hp,
-        this.enemyPrize = enemyPrize
+        this.hp = 50,
+        this.inventory = ["crossbow", "dagger", "axe"]   
+    }
+}
+console.log(`${name}, as you travel through the forest you must defend your life agains potential enemies. You are supplied with an inventory of weapons and you will have 100hp.`)
+
+// enemy information
+class Enemy{
+    constructor(enemyName, enemyHp, enemyTreasure){
+        this.enemyName = enemyName,
+        this.enemyHp = enemyHp,
+        this.enemyTreasure = enemyTreasure
     }
 }
 
-const madara = new Enemy("Madara", 80, "gold")
-const itachi = new Enemy("Itachi", 75, "food")
-const orochimaru = new Enemy("Orochimaru", 90, "sword")
+// enemy array, push new enemy into it
+const enemies = []
+const madara = new Enemy("Madara", 40, "gold")
+const itachi = new Enemy("Itachi", 30, "food")
+const orochimaru = new Enemy("Orochimaru", 25, "sword")
+enemies.push(madara, itachi, orochimaru)
+//console.log(enemies)
 
-const enemies = [madara, itachi, orochimaru]
+// player gets to choose between walking, printing an inventory or to exit the game.
+while(isAlive){
+    const option = readlineSync.keyIn("Now that you are all set up for your journey please make your selection: 'w' if you want to Walk, 'p' to print your Inventory, or 'q' to Exit the game.")
+    if(option === 'w'){
+        walk()
+    }else if (option === 'p'){
+        inventory()
+    }else if(option === 'q'){
+        isAlive = false
+        console.log("You ended your game. Good Bye!")
+    }
+}
 
-// player gets to choose to walk, print inventory or exit the game
-// while(isAlive){
-//     const action =
-// }
-
-// Functions 
-//**********************
+// random enemy selector function
+function randomEnemy(){
+    const enemyEncounter = enemies[Math.floor(Math.random()* enemies.length)]
+}
 
 // when enemy is defeated player gets a prize
-function enemyPrize(){
+function enemyTreasure(){
     const prize = ["gold", "food", "sword"]
     const randomPrize = randomPrize(0,2) // 1 out of 3 prizes available 
     const newPrize = prize[randomPrize]
@@ -66,8 +72,28 @@ function enemyPrize(){
     return newPrize
 }
 
-
-// random number function that picks a number in range
-const randomNum = function(max, min){
-    return Math.floor((Math.random()* max) + min)
+// player option functions: walk, print inventory, fight if enemy encountered
+function inventory(){
+    console.log(player);
+    walk()
 }
+
+function walk(){
+    const attackOrWalk = Math.random() * 100;
+    if(attackOrWalk <= 33){
+        randomEnemy();
+        console.log(enemyEncounter.enemyName, 'has attacked you!');
+        const fightOrRun = readlineSync.keyIn(`${name} your life is in danger! If you choose to defend yourself pick 'F' to fight or 'R' if you want to run like a coward!`);
+        if(fightOrRun === 'f'){
+            fight();
+        }else if(fightOrRun === 'r'){
+            run(); 
+        }
+    }else {
+        const travelOn =readlineSync.keyIn("You are a lucky bastard! You are yet to see another day of travel. To keep walking press 'w', to see your inventory status press 'p'");
+        if(travelOn === 'p'){
+            inventory();
+        }
+    }
+}
+
