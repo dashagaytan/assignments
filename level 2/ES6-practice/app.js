5354ee33c1f905d0cee41c5b68aac6734bf5edc5
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-07 12:51:59 -0500

    task 5: extra

--- level 2/ES6-practice/app.js
+++ level 2/ES6-practice/app.js
@@ -77,3 +77,27 @@ console.log(produceProduct(5, 5))
 
 const printString = (firstName = 'Jane', lastName = 'Doe', age = 100) => (`Hi ${firstName} ${lastName}, how does it feel to be ${age}?`)
 console.log(printString())
+
+// Task 5
+const animals = [
+    {
+        type: "dog",
+        name: "theodore"
+    },
+    {
+        type: "cat",
+        name: "whiskers"
+    },
+    {
+        type: "pig",
+        name: "piglette"
+    },
+    {
+        type: "dog",
+        name: "sparky"
+    }
+ ];
+ 
+ const filterForDogs = (arr) => arr.filter(animal => (animal.type === "dog"))
+ console.log(filterForDogs(animals))
+ 
