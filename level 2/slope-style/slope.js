544d8a589ef8b394a9c24715f4e9404958fbe7dc
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-07 13:23:30 -0500

    arrow function, array destructuring

--- level 2/slope-style/slope.js
+++ level 2/slope-style/slope.js
@@ -1,8 +1,28 @@
-// rest operator
+// 1. rest operator & arrow function
 
-function collectAnimals(...animals) {
-    return animals
-}
+const collectAnimals = (...animals) => animals
 
-collectAnimals("dog", "cat", "mouse", "jackolope", "platypus");
+console.log(collectAnimals("dog", "cat", "mouse", "jackolope", "platypus"));
 // ["dog", "cat", "mouse", "jackolope", "platypus"]
+
+// 2. Write a function that returns a food object with the array names as properties
+const  combineFruit = (fruit, sweets, vegetables) => ({fruit, sweets, vegetables})
+
+console.log(combineFruit(["apple", "pear"], ["cake", "pie"], ["carrot"]))
+
+// 
+
+const vacation = {
+  location: "Burly Idaho",
+  duration: "2 weeks"
+}
+ 
+const parseSentence = ({location, duration}) => `We're going to have a great time in ${location} for ${duration}.`
+
+console.log(parseSentence(vacation))
+
+// 3. array destructuring 
+function returnFirst(items){
+    const firstItem = items[0]; /*change this line to be es6*/
+    return firstItem
+}
