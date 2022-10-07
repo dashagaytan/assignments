3fb5ef115d61c3fc13b70593fa60f6dc43ee475a
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-07 12:57:54 -0500

    renamed files

---
+++ level 2/slope-style/slope.js
@@ -0,0 +1,8 @@
+// rest operator
+
+function collectAnimals(...animals) {
+    return animals
+}
+
+collectAnimals("dog", "cat", "mouse", "jackolope", "platypus");
+// ["dog", "cat", "mouse", "jackolope", "platypus"]
