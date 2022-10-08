a2d30195e984a17a47ae3ed8e5b330b8b93d09f9
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-07 22:06:00 -0500

    install of package json

--- level 2/princess-castle/castle.js
+++ level 2/princess-castle/castle.js
@@ -1,14 +1,40 @@
 const readlineSync = require("readline-sync")
-const status = ["Power Up", "Big", "Small", "Dead"]
+const statuses = ["Power Up", "Big", "Small", "Dead"]
 const setName = ["Mario", "Luigi"]
 
 
 class Player {
-    constructor(name, totalCoins, status, hasStars){
-        this.name = name;
+    constructor(name, totalCoins = 0, status = "", hasStar = false, gameActive = ture){
+        this.name = name
         this.totalCoins = totalCoins;
-        this.status = status;
-        this.hasStars = hasStars;
+        this.status = status
+        this.hasStar = hasStar;
+        this.gameActive = gameActive;
+    }
+    setName(){
+        const names = readlineSync.keyInSelect(namePicked)
+        if(names === 0){
+            this.name = "Mario "
+        }else if (names === 1){
+            this.name = "Luigi"
+        }
     }
 
+    gotHit(){
+        if(this.status === statuses[0] && this.hasStars === true){
+            console.log("Your star has protected you!")
+        }else if(this.status === statuses[0]){
+            this.status = statuses[1]
+            console.log("You got hit!")
+        }else if(this.status === statuses[1]){
+            this.status = statuses[2]
+            console.log("You got hit!")
+        }else if(this.status === statuses[2]){
+            this.status = statuses[3]
+            this.gameActive = false
+            console.log("You got hit!")
+            console.log("You lost the game. You died.")
+            console.log(`Your final score was ${this.totalCoins}`)
+        }
+    }
 }
