0a4652e2ed8d5da0784cddd89c27ce8abdf8519b
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-10 17:21:32 -0500

    updated showData function

--- level 2/Pokemon List/list.js
+++ level 2/Pokemon List/list.js
@@ -1,22 +1,21 @@
 // step one - get the data
 
 const xhr = new XMLHttpRequest()
-
 xhr.open("GET", "https://api.vschool.io/pokemon", true)
 xhr.send()
 
 //step two - display the data
-
 xhr.onreadystatechange = function(){
     if(xhr.readyState === 4 && xhr.status === 200){
         const JSONdata = xhr.responseText
         const data = JSON.parse(JSONdata)
-        console.log(data.results)
+        showData(data.objects[0].pokemon)
     }
 }
 
 function showData(arr){
     for(let i = 0; i < arr.length; i++){
+
         const h3 = document.createElement('h3')
         h3.textContent = arr[i].name
         document.body.appendChild(h3)
