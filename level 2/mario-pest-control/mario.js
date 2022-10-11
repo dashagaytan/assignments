e09e0ca9b958b10b18a25307299f2dbda73eec8a
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-11 12:33:02 -0500

    js logic

---
+++ level 2/mario-pest-control/mario.js
@@ -0,0 +1,21 @@
+// adding logic to calculate the total of baddies caught 
+const form = document.pestControl;
+// const btn = document.getElementsByClassName['btn']
+
+form.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const bobOmb = form.bobOmb.value;
+    const cheepCheep = form.cheepCheep.value;
+    const goomba = form.goomba.value;
+
+    let bobOmbPrice = bobOmb * 7 
+    let cheepCheepPrice = cheepCheep * 11
+    let goombaPrice = goomba * 5
+
+    const total = bobOmbPrice + cheepCheepPrice + goombaPrice
+
+    const totalPrice = document.createElement('p')
+    totalPrice.textContent = total + " Coins"
+    document.getElementsByClassName('totalPrice').append(totalPrice)
+})
