a2f81bf1aab4b7f05638e5219512bb77e39415bf
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-10-26 20:50:09 -0500

    added travel icon

--- level 3/vacation-spots/src/components/Card.js
+++ level 3/vacation-spots/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card(props){
     return(
         <div className="card"> 
-            <h2 className="card--place">Place: {props.place}</h2>
+            <h2 className="card--place"><img src ="https://img.icons8.com/office/16/undefined/marker.png"/> {props.place}</h2>
             <img className="places-img" src={props.img} alt="places"/>
             <h5 className="price">$ {props.price}</h5>
             <span className="timeToGo">Best time for travel: <b>{props.timeToGo}</b></span>
