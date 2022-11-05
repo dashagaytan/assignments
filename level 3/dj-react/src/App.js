2540a8267219342d9cce1f19809c0cac5290c5eb
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-11-05 17:48:50 -0500

    setting up child components

--- level 3/dj-react/src/App.js
+++ level 3/dj-react/src/App.js
@@ -2,12 +2,35 @@ import React from 'react';
 import Square from "./Square"
 
 function App() {
-  const [square, setSquare] = React.useState([white, white, white, white])
+  const [colors, setColors] = React.useState([white, white, white, white])
 
-  
+  function smallDJ(){
+    
+  }
+
+
   return (
     <div>
-        <Square />
+        <Square className="square1"/>
+        <Square className="square2"/>
+        <Square className="square3"/>
+        <Square className="square4"/>
+
+        <div>
+          <button className='btn1'></button>
+          <button lassName='btn2'></button>
+
+          <button lassName='btn3'></button>
+          <button lassName='btn4'></button>
+
+          <button lassName='btn5'></button>
+          <button lassName='btn6'></button>
+
+          <button lassName='btn7'></button>
+          <button lassName='btn8'></button>
+          <button lassName='btn9'></button>
+
+        </div>
     </div>
   );
 }
