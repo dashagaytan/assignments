35eac464401b922eb37abed4d96508be61a991a3
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-11-07 16:35:07 -0600

    display components to DOM

--- level 3/react-dice/src/App.js
+++ level 3/react-dice/src/App.js
@@ -6,18 +6,22 @@ import DiceBox from "./DiceBox"
 function App() {
   const [numbers, setNumbers] = React.useState([0, 0, 0, 0, 0])
 
-     function showRandomNum(){
-      
+    function rollRandomNum(){
+        setNumbers([
+          Math.floor(Math.random()* 6)
+        ])
     }
+    //map through each number in the array
+    const numberArray = numbers.map(num => <DiceBox randomNumber = {num} />)
 
     return(
         <div className="main-container">
           <h2 className="title">React Dice</h2>
         <div className="dies-container">
-            {}
+            {numberArray}
         </div>
           <div>
-              <Button handleClick={showRandomNum}/>
+              <Button handleClick={rollRandomNum}/>
           </div>
         </div>
     )
--- level 3/react-dice/src/DiceBox.js
+++ level 3/react-dice/src/DiceBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-
-export default function DiceBox(){
-    
+export default function DiceBox(props){
+    return(
+        <h5 className="face">{props.randomNumber}</h5>
+    )
 }
