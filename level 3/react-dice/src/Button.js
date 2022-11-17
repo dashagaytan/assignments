f945012bc214edbb6e3248657c134cc28a85b2a1
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-11-16 22:31:02 -0600

    init commit

--- level 3/react-dice/src/Button.js
+++ level 3/react-dice/src/Button.js
@@ -4,4 +4,4 @@ export default function Button(props){
     return(
         <button className="btn" onClick={props.handleClick}>Roll Dice</button>
     )
-}
+} 
--- level 3/react-dice/src/DiceBox.js
+++ level 3/react-dice/src/DiceBox.js
@@ -4,4 +4,4 @@ export default function DiceBox(props){
     return(
         <h5 className="face">{props.randomNumber}</h5>
     )
-}
+} 
