7f45a05e910e2479f3086cadda88676d5b2bab8d
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-12-02 20:51:57 -0600

    random color final styling

--- level 3/random-color/src/App.js
+++ level 3/random-color/src/App.js
@@ -1,20 +1,25 @@
-import React, { useReducer } from 'react'
+import React from 'react'
 import {useState, useEffect} from 'react'
 
 function App() {
-  const [color, setColor] = useState({})
-  // const [randomColor, setRandomColor] = useState({})
+  const [color, setColor] = useState('')
+  const [randomColor, setRandomColor] = useState('')
+  // const url = "https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}"
 
   useEffect(()=> {
     fetch(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()} ${color}`)
     .then(res =>res.json())
+    .then(data => setColor(data.colors[0].hex))
     .catch(err => console.log(err))
-  })
+  }, [])
+
+  function handleClick(){
+    setRandomColor(color)
+  }
 
   return (
-    <div className='container'>
-      <h1> Random Color</h1>
-      <button onClick={() => setColor(prevColor => prevColor)}>Next</button>
+    <div className='container' style={{backgroundColor: `#${randomColor}`}}>
+      <button onClick={handleClick}>Get Random Color</button>
     </div>
   );
 }
