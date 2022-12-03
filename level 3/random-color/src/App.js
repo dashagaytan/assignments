11e21183cdff5e4063460f800353f8bc926439dc
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-12-02 20:25:07 -0600

    using axios, GET random color from API

--- level 3/random-color/src/App.js
+++ level 3/random-color/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [color, setColor] = useState({})
+  // const [randomColor, setRandomColor] = useState({})
+
+  useEffect(()=> {
+    fetch(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()} ${color}`)
+    .then(res =>res.json())
+    .catch(err => console.log(err))
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1> Random Color</h1>
+      <button onClick={() => setColor(prevColor => prevColor)}>Next</button>
     </div>
   );
 }
