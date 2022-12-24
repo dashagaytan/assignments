cc1070d68145ae1fba3c809290d5fbc901c6e01c
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-12-24 13:39:21 -0600

    init commit

---
+++ level 3/caesar-cipher/App.js
@@ -0,0 +1,4 @@
+var readline = require('readline-sync');
+var input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
+var shift = parseInt(readline.question('How many letters would you like to shift? '));
+
