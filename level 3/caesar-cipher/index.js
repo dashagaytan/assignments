ae453e1d1fce58c857179969a8598df815b4b351
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-01-04 13:23:01 -0600

    caesar cipher

--- level 3/caesar-cipher/index.js
+++ level 3/caesar-cipher/index.js
@@ -1,4 +1,16 @@
-var readline = require('readline-sync');
-var input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
-var shift = parseInt(readline.question('How many letters would you like to shift? '));
+let readline = require('readline-sync');
+let input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
+let shift = parseInt(readline.question('How many letters would you like to shift? '));
+let alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
 
+//mapping over input in an array
+const cipher = input.split('').map(char => {
+    if(alphabet.includes(char)){
+        let index = (alphabet.indexOf(cahr) + shift) %26
+        return alphabet[index]
+    }else{
+        return char;
+    }
+})
+
+console.log(`Secret message is: ${cipher}`)
