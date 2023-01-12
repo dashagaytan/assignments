334452f81572721d577d95510bfa15b1f5964feb
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-01-11 20:52:52 -0600

    setup

--- level 4/gradient-generator/src/App.js
+++ level 4/gradient-generator/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import React from "react";
+import Generator from "./Generator";
 
 function App() {
   return (
     <div>
-      
+      <Generator>
+        <h1>CSS Gradient Code Generator</h1>
+      </Generator>
     </div>
   );
 }
