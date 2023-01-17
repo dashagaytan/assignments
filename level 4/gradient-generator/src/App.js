7d8aea53adacaa02441b9a4a9257153dc44ca58d
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-01-17 13:25:28 -0600

    set up gradietn layput

--- level 4/gradient-generator/src/App.js
+++ level 4/gradient-generator/src/App.js
@@ -2,10 +2,71 @@ import React from "react";
 import Generator from "./Generator";
 
 function App() {
+  const [gradient, setGradient] = React.useState({
+    color1: '#ffffff',
+    color2: '#000000',
+    angle: '0'
+  })
+
+function handleChange(event){
+  const {name, value}= event.target
+  setGradient(prevGradient => {
+    return{
+      ...prevGradient,
+      [name]: value
+    }
+  })
+  console.log(gradient)
+}
+
+// setting a cross-browser CSS code in a text box that user will be able to generate and copy. 
+let cssColorCode = ``
+
   return (
     <div>
+        <h1 className="title">CSS Gradient Code Generator</h1>
+        {/* gradient 2 color options, and angle */}
       <Generator>
-        <h1>CSS Gradient Code Generator</h1>
+        <h2>Gradient Options:</h2>
+          <form className="form">
+            <h3>Color 1:</h3>
+            <h3>{gradient.color1}</h3>
+              <input 
+              type="color"
+              name="color1"
+              value={gradient.color1}
+              onChange={handleChange}
+              className="color-input"
+              />
+            <h3>Color 2:</h3>
+            <h3>{gradient.color2}</h3>
+              <input 
+              type="color"
+              name="color2"
+              value={gradient.color2}
+              onChange={handleChange}
+              className="color-input"
+              />
+
+            <h3>Angle</h3>
+              <input
+              type="number"
+              name="angle"
+              value={gradient.angle}
+              className="angle-input"
+              onChange={handleChange}
+              />
+          </form>
+      </Generator>
+
+      {/* gradient box with css code for user to copy */}
+      <Generator>
+        <div  className="gradient-box">
+          <div className="code-box" style={{}}>
+    
+          </div>
+          <textarea readOnly value={cssColorCode}></textarea>
+        </div>
       </Generator>
     </div>
   );
