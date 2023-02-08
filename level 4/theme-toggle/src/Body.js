e73666a6a152508911a1f84ba37764a85e5ec033
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-02-07 18:26:37 -0600

    added changes to body

--- level 4/theme-toggle/src/Body.js
+++ level 4/theme-toggle/src/Body.js
@@ -8,7 +8,7 @@ function Body(props){
         <div className={`${theme}-theme`}>
           
             <div className="body-container">
-                <h1 className="title">Change Theme from Light to Dark</h1>
+                <h1 className="title">THEME TOGGLE</h1>
                 <p className="body-p">On click of a button you can change the theme from dark to light</p>
 
                 <button onClick={toggleTheme}>Click me</button>
