5b5a966116efc3b173f9ecc5f66f3c9652ec1102
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-03-17 17:25:22 -0500

    list component setup

--- level 4/ugly-things/src/components/UglyContext.js
+++ level 4/ugly-things/src/components/UglyContext.js
@@ -7,16 +7,13 @@ const UglyContext = React.createContext();
 //form state 
 function UglyThingsProvider(props){
     
+    const [uglyArray, setUglyArray] = useState([])
+    
     const [uglyThing, setUglyThing] = useState({
         title: "",
         description: "",
         imgUrl: ""
     })
-    const [uglyArray, setUglyArray] = useState([])
-    const [editOption, setEditOption] = useState(false)
-    const [editUgly, setEditUgly] = useState({title: "", description: ""})
-    
-    // const [uglyArr, setUglyArr] = useState([])
         
     //handles user input
     function handleChange(event){
@@ -30,31 +27,32 @@ function UglyThingsProvider(props){
 
     //get images from and API and set useEffect on first render
     function getUglyThings(){
-        fetch("https://api.vschool.io/dashagaytan/thing")
-        .then(res => res.json())
-        .then(data => {
-            setUglyArray(() => [...data])
-        })
+        axios.get("https://api.vschool.io/dashagaytan/thing")
+        .then(res =>{
+            console.log(res.data)
+             setUglyArray((res.data))
+        })  
         .catch(err => console.log(err))
     }
 
     useEffect(() => {
-        getUglyThings()
+        getUglyThings() 
     }, [])
 
-    //submits form on click Submit
-    function handleSubmit(event){
-        event.preventDefault()
-        const getNewUgly = {...uglyThing}
-        setUglyThing(() => ({
-            title: "",
-            description: "",
-            imgUrl: ""
-        }))
-            axios.post("https://api.vschool.io/dashagaytan/thing/" , getNewUgly)
-            .then(() => getUglyThings())
-            .catch(err => console.log(err))
-        }
+        //submits form on click Submit
+        function handleSubmit(event){
+            event.preventDefault()
+            // const getNewUgly = {...uglyThing}
+            // setUglyThing(() => ({
+            //     title: "",
+            //     description: "",
+            //     imgUrl: ""
+            // }))
+                axios.post("https://api.vschool.io/dashagaytan/thing/" , uglyThing)
+                .then(() => getUglyThings())
+                .catch(err => console.log(err))
+            }
+    
     
         function handleDelete(id){
             axios.delete(`https://api.vschool.io/dashagaytan/thing/${id}`)
@@ -62,39 +60,14 @@ function UglyThingsProvider(props){
             .catch(err => console.log(err))
         }
 
-        //watches for changes on edit
-        function handleEditChange(event){
-            const {name, value} = event.target
-            setEditUgly( prevState => ({
-                ...prevState,
-                [name]: value
-            }))
-        }
-
-        //edit option to change title and description of an image in the list
-        function handleEdit(id, ugly){
-            console.log(id, ugly)
-            axios.put("")
-        }
-
-        //saves the edited changes of an existing image
-        function handleSave(){
-            
-        }
-
     return (
         <UglyContext.Provider value ={{
              uglyThing,
              uglyArray,
-             editOption,
-             editUgly,
-             handleEdit,
              handleDelete,
-             handleSave,
              getUglyThings,
              handleChange, 
              handleSubmit,
-             handleEditChange
              }}>
             {props.children}
         </UglyContext.Provider>
--- level 4/ugly-things/src/components/UglyList.js
+++ level 4/ugly-things/src/components/UglyList.js
@@ -2,12 +2,10 @@ import React, {useContext} from "react";
 import { UglyContext } from "./UglyContext";
 
 function UglyList(props){
-    const {uglyThing} = useContext(UglyContext)
-
   
     return(
         <div className="uglyList">
-            {uglyThing}
+         
         </div>
     )
 }
