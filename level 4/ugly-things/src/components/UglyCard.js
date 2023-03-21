9771b7a94d7db958cec1e8dd35bc85a18c946a23
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-03-21 15:25:14 -0500

    created a card component

--- level 4/ugly-things/src/components/UglyCard.js
+++ level 4/ugly-things/src/components/UglyCard.js
@@ -3,6 +3,15 @@ import { UglyContext } from "./UglyContext";
 
 function UglyCard(prop){
     const {handleDelete,} = useContext(UglyContext)
+
+//     <div className="uglyCard">
+//     <h1 className="title">{item.title}</h1>
+//     <img src={item.imgUrl} alt=""/>
+//     <h2 className="description">{item.description}</h2>
+// </div>
+// <div>
+//     <button className="deleteBtn" onClick={()=>handleDelete(item.id)}>Delete</button>
+// </div>
 }
 
 export default UglyCard;
--- level 4/ugly-things/src/components/UglyList.js
+++ level 4/ugly-things/src/components/UglyList.js
@@ -2,28 +2,22 @@ import React, {useContext} from "react";
 import UglyCard from "./UglyCard";
 import { UglyContext } from "./UglyContext";
 
-function UglyList(){
-  const { uglyArray, handleDelete} = useContext(UglyContext)
+export default function UglyList(){
+  const { uglyArray} = useContext(UglyContext)
 
-const uglyThings = uglyArray.map(item => {
-    return(
-        <UglyCard
-            <div className="uglyCard">
-                <h1 className="title">{item.title}</h1>
-                <img src={item.imgUrl} alt=""/>
-                <h2 className="description">{item.description}</h2>
-            </div>
-            <div>
-                <button className="deleteBtn" onClick={()=>handleDelete(item.id)}>Delete</button>
-            </div>
-        />
-    )
-})
-    return(
-        <div className="uglyList">
-            {uglyThings}
-        </div>
-    )
+return (
+    <div>
+        {uglyArray.map(item => {
+                return (
+                    <UglyCard 
+                    key ={item.id}
+                    id ={item.id}
+                    title={item.title}
+                    description = {item.description}
+                    imgUrl ={item.imgUrl}
+                    />
+                )
+            })}
+    </div>
+)
 }
-
-export default UglyList;
