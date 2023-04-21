c68e5e7926549c062417ec125abd46662577b7f4
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-04-21 15:43:54 -0500

    added simple style to nav bar

--- level 4/react-roto-router/src/App.js
+++ level 4/react-roto-router/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import Home from "./components/Home"
+import Home from "./components/Home.js"
+import About from "./components/About.js"
+import Services from "./components/Services.js"
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/" >Home</Link>
+
+      <nav className="navBar">
+        <h2>Plumbing Co.</h2>
+        <Link className="link" to="/" style={{ padding: 5 }}>Home</Link>
+        <Link className="link" to="/about" style={{ padding: 5 }}>About</Link>
+        <Link className="link" to="/services" style={{ padding: 5 }}>Services</Link>
       </nav>
+
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/services" element={<Services />}/>
         </Routes>
     </Router>
   );
