513bbf255532fc4b9f309e90ce0e95c1adc0a5fc
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-04-22 22:31:33 -0500

    completed home page

--- level 4/react-roto-router/src/components/Home.js
+++ level 4/react-roto-router/src/components/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 
 function Home(){
+
+    const navigate = useNavigate()
+
     return(
-        <div>
+        <div className="home">
             <h2 className="home-title">Welcome to The Plumbing Co.</h2>
-                <h4>Here we take care of your problems </h4>
-                
+            <h4>Here we take care of your problems </h4>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> 
+            <p>Nulla lobortis magna nibh, vel sollicitudin sapien bibendum eu. 
+            Fusce lacinia interdum ex, sit amet eleifend justo. </p>
+            <p>Praesent tincidunt bibendum ante, at elementum eros auctor eget. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+            Vivamus sit amet semper est.</p>
+            
+            <button style={{display: "block", margin: "0 auto", marginTop: "20px"}} onClick={()=> navigate("/services")}>Request Services</button>
+            <br></br>
+            <button style={{display: "block", margin: "0 auto", marginTop: "20px"}} onClick={()=> navigate("/about")}>Get to know us</button>
         </div>
     )
 }
--- level 4/react-roto-router/src/components/Services.js
+++ level 4/react-roto-router/src/components/Services.js
@@ -1,22 +1,26 @@
 import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Services(){
+    const navigate = useNavigate()
+
     return(
-        <div className="form-container">
+        <div>
+            <div className="form-container">
             <form>
             <h2 className="serv-title">Fast and reliable Plumbing Srvices</h2>
             <p className="serv-p">Requst More Information</p>
                 <input
                 className="input"
                 type="text"
-                name="name" required
+                name="name"
                 placeholder="Your Name"
                 />
 
                 <input
                 className="input"
                 type="email"
-                name="email" required
+                name="email" 
                 placeholder="Email"
                 />
 
@@ -28,6 +32,8 @@ function Services(){
 
                 <button>Submit</button>
             </form>
+           </div>
+           <button style={{display: "block", margin: "0 auto", marginTop: "20px"}} onClick={()=> navigate("/")}>Home</button>
         </div>
     )
 }
