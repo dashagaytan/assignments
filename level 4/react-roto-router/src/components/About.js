8fb6c529571f4e17046e81fd5b5426c7936cbdd0
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-04-22 22:05:16 -0500

    added navigation btn to about page

--- level 4/react-roto-router/src/components/About.js
+++ level 4/react-roto-router/src/components/About.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 
 function About(){
+
+    const navigate = useNavigate()
+
     return(
         <div>
-            <h2>About Component</h2>
+            <div className="about">
+                <h2>The Plumbing Co.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Nulla lobortis magna nibh, vel sollicitudin sapien bibendum eu. 
+                    Fusce lacinia interdum ex, sit amet eleifend justo. 
+                    Praesent tincidunt bibendum ante, at elementum eros auctor eget. 
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+                    Vivamus sit amet semper est.</p>
+            </div>
+            <button style={{display: "block", margin: "0 auto", marginTop: "20px"}} onClick ={()=>navigate("/services")}>Request Services</button>
         </div>
     )
 }
