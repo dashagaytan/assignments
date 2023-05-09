ffd93084e90fb6510c945de002d997cf4cd4ed71
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-05-08 21:28:40 -0500

    commit

--- level 4/meal-nanny/src/components/RandomMeal.js
+++ level 4/meal-nanny/src/components/RandomMeal.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import "./component-styles/RandomMeal.css"
 
 function RandomMeal(){
 
@@ -20,12 +21,25 @@ function RandomMeal(){
         <div className="randomMeal-container">
             {randomMeal.map(meal =>(
                <div key={meal.idMeal} >
-                <div className="random-meal">
-                    <h2>{meal.strMeal}</h2>
-                    <img src={meal.strMealThumb} alt="meal" style={{width: "200px", height: "200px"}}/>
-                    <p>{meal.strCategory}</p>
-                </div>
-                    <button onClick={getRandomMeal}>Meal Nanny show next meal</button>
+                    <div className="random-meal">
+                        <h2 className="randomMeal-name">{meal.strMeal}</h2>
+                        <img className="randomMeal-img" src={meal.strMealThumb} alt="meal" style={{ width: "500px", height: "auto", borderRadius: "10px" }}/>
+                        <p className="randomMeal-category">{meal.strTags}</p>
+                        <button className="randomMeal-btn" onClick={getRandomMeal}>Meal Nanny show another meal</button>
+                            <div className="details-container">
+                                <div>
+                                    <h3 className="ingredients-title">Ingredients Needed:</h3>
+                                    <p className="random-ingredients">{meal.strIngredient1} | {meal.strMeasure1}</p>
+                                    <p className="random-ingredients">{meal.strIngredient2} | {meal.strMeasure2}</p>
+                                    <p className="random-ingredients">{meal.strIngredient3} | {meal.strMeasure3}</p>
+                                    <p className="random-ingredients">{meal.strIngredient4} | {meal.strMeasure4}</p>
+                                    <p className="random-ingredients">{meal.strIngredient5} | {meal.strMeasure5}</p>
+                                    <p className="random-ingredients">{meal.strIngredient6} | {meal.strMeasure6}</p>
+                                    <p className="random-ingredients">{meal.strIngredient5} | {meal.strMeasure7}</p>
+                                    <p className="random-ingredients">{meal.strIngredient6} | {meal.strMeasure8}</p>
+                                </div>
+                            </div>
+                    </div>
                </div> 
             ))}
         </div>
