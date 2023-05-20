0c7105a694fc00951ba49030ca07d2e89e278024
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-05-19 21:36:52 -0500

    complete, last commit

--- level 5/bounty-hunter-pt1/server.js
+++ level 5/bounty-hunter-pt1/server.js
@@ -1,9 +1,28 @@
 const express = require("express")
+const {v4: uuidv4} = require('uuid')
 
 const app = express()
 
+//Middleware (for every request)
 app.use(express.json())
 
+const bounties = [
+    {firstName: "Luke", lastName: "Skywalker", living: true, bounty: 20490 ,type: 'Jedi', _id: uuidv4()},
+    {firstName: "Darth", lastName: "Sidious", living: false, bounty: 0, type: 'Sith', _id: uuidv4()},
+    {firstName: "Darth", lastName: "Maul", living: false, bounty: 0, type: 'Sith', _id: uuidv4()},
+    {firstName: "Obi-Wan", lastName: "Kenobi", living: true, bounty: 543400, type: 'Jedi', _id: uuidv4()}
+]
+
+app.get("/bounties", (req, res)=>{
+    res.send(bounties)
+})
+
+app.post("/bounties", (req, res)=>{
+    const newBounty = req.body
+    newBounty._id = uuidv4();
+    bounties.push(newBounty)
+    res.send(`${newBounty.firstName} was added to your API`)
+})
 
 
 
