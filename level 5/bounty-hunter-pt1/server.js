22a3af9dc15334aab079d2cff7b5debcac56210f
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-05-19 21:06:47 -0500

    created routes for bounty

--- level 5/bounty-hunter-pt1/server.js
+++ level 5/bounty-hunter-pt1/server.js
@@ -3,3 +3,12 @@ const express = require("express")
 const app = express()
 
 app.use(express.json())
+
+
+
+
+app.listen(9000, ()=>{
+    console.log("Server is running on port9000")
+})
+
+module.exportts = express
