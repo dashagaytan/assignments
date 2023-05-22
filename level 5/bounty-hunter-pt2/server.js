2b54bab53cb29ab9a14d0d2390fbb54f910fa600
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-05-22 14:35:33 -0500

    init setup bounty hunter pt 2

--- level 5/bounty-hunter-pt2/server.js
+++ level 5/bounty-hunter-pt2/server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const {v4: uuidv4} = require('uuid')
-
 const app = express()
 
-//Middleware (for every request)
+//middleware for every request
 app.use(express.json())
 
+//array of objects / Fake data
 const bounties = [
     {firstName: "Luke", lastName: "Skywalker", living: true, bounty: 20490 ,type: 'Jedi', _id: uuidv4()},
     {firstName: "Darth", lastName: "Sidious", living: false, bounty: 0, type: 'Sith', _id: uuidv4()},
@@ -17,17 +17,6 @@ app.get("/bounties", (req, res)=>{
     res.send(bounties)
 })
 
-app.post("/bounties", (req, res)=>{
-    const newBounty = req.body
-    newBounty._id = uuidv4();
-    bounties.push(newBounty)
-    res.send(`${newBounty.firstName} was added to your API`)
+app.listen(8000, ()=> {
+    console.log("Server is running on port 8000")
 })
-
-
-
-app.listen(9000, ()=>{
-    console.log("Server is running on port 9000")
-})
-Se.
-module.exportts = express
