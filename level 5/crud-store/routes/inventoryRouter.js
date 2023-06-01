cc376116186b3e51b12f3e9fdcbe4e33f010ea67
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-06-01 10:37:26 -0500

    added all CRUD methods

--- level 5/crud-store/routes/inventoryRouter.js
+++ level 5/crud-store/routes/inventoryRouter.js
@@ -31,11 +31,52 @@ inventoryRouter.get("/", (req, res, next)=> {
             res.status(200).send(foundItem)
         })
     })
-    
+
 //Post One
+    inventoryRouter.post("/", (req, res, next) => {
+        const newItem = new Inventory(req.body);
+        newItem.save()
+        .then(savedItem=>{
+            return res.status(200).send(savedItem);
+        })
+        .catch(err => {
+            res.status(500)
+            return next(err)
+        })
+    })
 
 //Update One
+inventoryRouter.put("/:itemId", (req, res) => {
+    Inventory.findOneAndUpdate(
+        {_id: req.params.itemId},   
+        req.body,                        
+        { new: true })                   
+            .then((updatedItem)=> {
+            if(!updatedItem){
+                return res.status(404).send("Item not found")
+            }
+            return res.status(200).send(updatedItem)
+        })
+        .catch((err)=> {
+            res.status(500)
+            return next(err)
+        })
+})
 
 //Delete One
+inventoryRouter.delete("/:itemId", (req, res, next)=>{
+    Inventory.findOneAndDelete(
+        {_id: req.params.itemId})
+        .then((deletedItem)=> {
+            if(!deletedItem){
+                return res.status(404).send("Item not found");
+            }
+            return res.status(200).send(`Successfully deleted item ${deletedItem.name} from the database`)
+        })
+        .catch((err)=>{
+            res.status(500)
+            return next(err)
+        })
+})
 
 module.exports = inventoryRouter
