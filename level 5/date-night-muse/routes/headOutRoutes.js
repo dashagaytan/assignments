ea2ccbfe777aa5b097086ec8699e212fc3fea8c1
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-06-04 18:50:11 -0500

    routes setup

---
+++ level 5/date-night-muse/routes/headOutRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const headOutRouter = express.Router();
+const HeadOut = require('../models/headOut')
+
+//GET All
+
+//Get one
+
+//Get by category
+
+module.exports = headOutRouter
---
+++ level 5/date-night-muse/routes/stayInRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const stayInRouter = express.Router();
+const StayIn = require('../models/stayIn')
+
+//GET All
+
+//Get one
+
+//Get by category
+
+module.exports = stayInRouter
