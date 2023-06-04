9735a5c91fe4de9d1f03876780fd5fd06aff8203
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-06-04 18:15:27 -0500

    models

--- level 5/date-night-muse/models/headOut.js
+++ level 5/date-night-muse/models/headOut.js
@@ -3,7 +3,22 @@ const Schema = mongoose.Schema
 
 // Head out Date Night Muse bluepring 
 const headOutSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      category: {
+        type: String,
+        required: true,
+      },
 })
 
 module.exports = mongoose.module("HeadOut", headOutSchema)
--- level 5/date-night-muse/models/stayIn.js
+++ level 5/date-night-muse/models/stayIn.js
@@ -3,7 +3,18 @@ const Schema = mongoose.Schema
 
 // Stay In Date Night Muse bluepring 
 const stayInSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      category: {
+        type: String,
+        required: true,
+      },
 })
 
 module.exports = mongoose.module("StayIn", stayInSchema)
