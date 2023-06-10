84234a97aedff77cff6d59891d0b11a89556aa7d
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-06-09 21:33:58 -0500

    init server setup

--- level 5/date-night-muse/routes/stayInRoutes.js
+++ level 5/date-night-muse/routes/stayInRoutes.js
@@ -27,16 +27,27 @@ stayInRouter.get("/search/category", (req, res, next)=> {
 })
 
 //Post One (not available for users)
-stayInRouter.post("/", (req, res, next)=>{
-    const newStayIn = new StayIn(req.body);
-    newStayIn.save()
-      .then(saveStayIn => {
-        return res.status(201).send(saveStayIn);
-      })
-      .catch(err => {
-        res.status(500);
-        return next(err);
-      });
+stayInRouter.post("/", (req, res, next)=> {
+    const stayIn = new StayIn(req.body)
+    stayIn.save((err, items)=>{
+        if(err) { 
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(items)
+    } )
 })
+
+// stayInRouter.post("/", (req, res, next)=>{
+//     const newStayIn = new StayIn(req.body);
+//     newStayIn.save()
+//       .then(saveStayIn => {
+//         return res.status(201).send(saveStayIn);
+//       })
+//       .catch(err => {
+//         res.status(500);
+//         return next(err);
+//       });
+// })
 
 module.exports = stayInRouter;
