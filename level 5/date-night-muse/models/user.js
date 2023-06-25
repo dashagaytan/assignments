1139d522881219f91764e6408ca21fbd1a5e600e
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-06-25 17:17:58 -0500

    user model updated

--- level 5/date-night-muse/models/user.js
+++ level 5/date-night-muse/models/user.js
@@ -23,3 +23,30 @@ const userSchema = new Schema({
         default: false
     }
 })
+
+// pre-saved hook to encrypt user password on signup
+userSchema.pre("save", function(next){
+    if(!this.isModified("password")) return next()
+    bcrypt.hash(this.password, 10, (err, hash)=> {
+        if(err) return next(err)
+        this.password = hash
+        next()
+    })
+})
+
+// method to check envrypted password on login
+userSchema.methods.checkPassword = function(passwordAttempt, callback){
+    bcrypt.compare(passwordAttempt, this.password, (err, isMatch) => {
+        if(err) return callback(err)
+        return callback(null, isMatch)
+    })
+}
+
+// method to remove user's password for tocken/sending the response
+userSchema.methods.withoutPassword = function(){
+    const user = this.toObject()
+    delete user.password
+    return user;
+}
+
+module.exports = mongoose("User", userSchema)
